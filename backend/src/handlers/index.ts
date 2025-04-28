f2222e89a45e203b479608003e463d3b6c875fde
Compadre00 <oscar.raya2s@hotmail.com>
2025-04-28 14:48:15 -0600

    save 2

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import slug from 'slug'
 import formidable from "formidable";
+import {v4 as uuid} from 'uuid'
 import User from "../models/User";
 import { checkPassword, hashPassword } from "../utils/auth"
 import { generateJWT } from "../utils/jwt";
@@ -107,11 +108,25 @@ export const uploadImage = async(req: Request, res: Response) => {
 
     const form = formidable({multiples: false});
 
-    form.parse(req, (error, fields, files) => {
-        console.log(files.file[0].filepath);
-    })
-
     try {
+        form.parse(req, (error, fields, files) => {
+            console.log(files.file[0].filepath);
+
+            cloudinary.uploader.upload(files.file[0].filepath, {public_id: uuid()}, async function(error, result) {
+                if (error) {
+                    const error = new Error('Hubo un error al subir la imagen');
+                    res.status(500).json({error : error.message})   
+                }
+                if (result) {
+                    req.user.image = result.secure_url;
+                    await req.user.save();
+                    res.json({image: result.secure_url})
+                }
+                
+            })
+        })
+
+        
         
     } catch (e) {
         const error = new Error('Hubo un error');
